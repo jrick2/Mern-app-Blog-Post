ece72d9902352db68dd73162f99cb0758e6cc66d
jrick2 <joshrick16.abellera@gmail.com>
2023-02-28 20:01:53 -0600

    starter

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import connectDB from "./utils/connectDB";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -10,7 +10,12 @@ app.use(express.json());
 const port = process.env.PORT || 1111;
 
 app.listen(port, async () => {
-  console.log(`Server is lisining on port ${port}....`);
-
-  await connectDB();
+  try {
+    console.log(`Server is lisining on port ${port}....`);
+    await mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.DB_URI);
+    console.log("Connected To DB");
+  } catch (error) {
+    console.error(error, "Failed To Start Server");
+  }
 });
